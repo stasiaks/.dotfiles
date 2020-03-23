ed1077a5d2e18128e2d2138e4d893427ab06c886
Sebastian Stasiak <arionw@live.com>
2020-03-23 19:21:59 +0100

    Screen layouts

---
+++ screenlayout/workstation-pc.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-A-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-D-0 --off
--- xmonad/scripts/autostart.sh
+++ xmonad/scripts/autostart.sh
@@ -8,4 +8,12 @@ function run {
   fi
 }
 
+LAYOUT_DIRECTORY=$HOME/.screenlayout
+LAYOUT_SCRIPT=$LAYOUT_DIRECTORY/current.sh
+
+if [ -f "$LAYOUT_SCRIPT" ]; then
+	run $LAYOUT_SCRIPT
+fi
+
 (sleep 2; run $HOME/.config/polybar/launch.sh) &
+
