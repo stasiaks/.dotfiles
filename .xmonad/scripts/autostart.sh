ad2cd07edfc32757ecb68b3c444041838b049aec
Sebastian Stasiak <arionw@live.com>
2024-12-15 19:56:17 +0100

    TeXLive

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -8,13 +8,7 @@ function run {
   fi
 }
 
-LAYOUT_DIRECTORY=$HOME/.screenlayout
-LAYOUT_SCRIPT=$LAYOUT_DIRECTORY/set-layout.sh
-
-if [ -f "$LAYOUT_SCRIPT" ]; then
-	run $LAYOUT_SCRIPT
-fi
-
+(sleep 5; run autorandr --change) &
 (sleep 2; run $HOME/.config/polybar/launch.sh) &
 
 run compton --active-opacity 1.0 --shadow-ignore-shaped
